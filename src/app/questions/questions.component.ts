076888596757573ed56693a15603d4e99322acd3
leandro <id.leandrolima@gmail.com>
2017-12-16 18:06:00 -0200

    'implementação'

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -15,47 +15,109 @@ export class QuestionsComponent implements OnInit {
 	public questions
 	public objeto = []
 	public options = []
-	play: boolean = true
+	play = false
 
 	constructor(private questionsService: QuestionsService, private route: ActivatedRoute) {}
 
-	ngOnInit() { this.questionsService.questionById(this.route.snapshot.params['id']).subscribe(question => this.questions = question, request => console.log('Deu erro'), () => console.log('tudo ok')) }
+	ngOnInit() { this.questionsService.questionById(this.route.snapshot.params['id']).subscribe(question => this.questions = question, request => console.log('Deu erro'), () => this.start()) }
 
 	start() {
 
-		this.play  = true
 		var alternatives = this.questions.alternatives
-		var questions = []
+		var options = []
 
-		var actives = []
 
 		for (var i in alternatives) {
 
 			if(alternatives[i].ativo === undefined && alternatives[i].ativo != 'nao')  {
-				console.log('entrou para não')
 				this.questions.alternatives.map(q => [q, q.ativo = "nao", q.respondeu = "nao" ]);
 				return false
-
 			} 
 
 			if(alternatives[i].ativo == 'nao' && alternatives[i].respondeu == 'nao' ) {
+				this.update()
 				this.questions.alternatives[i].ativo = 'sim'
-				console.log('entrou para sim')
-				console.log(this.questions.alternatives[i])
+				this.objeto.push(this.questions.alternatives[i])
+				this.extract();
 				return false
 			}
 
-	
+
 			if(alternatives[i].ativo == 'sim' && alternatives[i].respondeu == 'nao') {
+				this.update()
 				this.questions.alternatives[i].ativo = 'nao'
 				this.questions.alternatives[i].respondeu = 'sim'
-				console.log('respondeu')
-				console.log(this.questions.alternatives[i])
+				this.objeto.push(this.questions.alternatives[i])
+				this.extract();
 				return false
 			}
 
 		}
 	}
+
+
+	extract() {
+		this.options = []
+		for (var i in this.objeto) {
+			console.log('mirra' + this.questions.name)
+			for (var j in this.objeto[i].options) {
+
+				console.log(this.objeto[i].options[j].optionA)
+				console.log(this.objeto[i].options[j].optionB)
+				console.log(this.objeto[i].options[j].optionC)
+				console.log(this.objeto[i].options[j].optionC)
+
+				for (var k in this.objeto[i].options[j]) {
+					this.options.push(this.objeto[i].options[j][k])
+				}
+
+			}
+		}
+	}
+
+
+	update(){
+		this.objeto = []
+	}
+
+
+	continuar(){
+		this.play = true
+		this.start()
+	}
+
+
+
+
+
+
+
+// var myObj, i, j, x = "";
+// myObj = {
+//     "name":"John",
+//     "age":30,
+//     "cars": [
+//         { "name":"Ford", "models":[ "Fiesta", "Focus", "Mustang" ] },
+//         { "name":"BMW", "models":[ "320", "X3", "X5" ] },
+//         { "name":"Fiat", "models":[ "500", "Panda" ] }
+//     ]
+// }
+
+// for (i in myObj.cars) {
+//     x += "<h2>" + myObj.cars[i].name + "</h2>";
+//     for (j in myObj.cars[i].models) {
+//         x += myObj.cars[i].models[j] + "<br>";
+//     }
+// }
+
+
+
+
+
+
+
+
+
 
 
 
